d049856f9881488c5b7fba73f30272b9790c3794
Michael Todd <52994476+mtoddx@users.noreply.github.com>
2024-12-18 14:11:19 +1100

    icon

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     });
 
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/static");
   
     return {
       dir: {
